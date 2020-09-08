d2627ebb397ec89b9fbfa7487f48928cb351d43e
MAJESTY <ugochukwuaugustine7@gmail.com>
2020-09-07 18:46:43 -0700

    another commit

--- public/assets/js/teacher.js
+++ public/assets/js/teacher.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 $(function () {
     $('input, select').on('focus', function () {
         $(this).parent().find('.input-group-text').css('border-color', '#80bdff');
@@ -5,4 +6,24 @@ $(function () {
     $('input, select').on('blur', function () {
         $(this).parent().find('.input-group-text').css('border-color', '#ced4da');
     });
+});
+
+var f_curse_adviser = document.getElementById('f_curse_adviser');
+var f_admin = document.getElementById('f_admin');
+var btn_adviser_navigator = document.getElementById('btn_adviser_navigator');
+var btn_admin_navigator = document.getElementById('btn_admin_navigator');
+
+f_curse_adviser.style.display = 'block';
+f_admin.style.display = 'none';
+
+btn_admin_navigator.addEventListener('click', () => {
+    f_curse_adviser.style.display = 'block';
+    f_admin.style.display = 'none';
+})
+
+btn_adviser_navigator.addEventListener('click', () => {
+    f_admin.style.display = 'block';
+    f_curse_adviser.style.display = 'none';
+})
+
 });
