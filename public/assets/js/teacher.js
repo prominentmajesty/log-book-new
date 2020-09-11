e620a0030a379517a2fd73e1168c5cd23f897859
MAJESTY <ugochukwuaugustine7@gmail.com>
2020-09-11 13:24:24 -0700

    Another Commit

--- public/assets/js/teacher.js
+++ public/assets/js/teacher.js
@@ -8,18 +8,40 @@ $(document).ready(function(){
         });
     });
 
+    //var for Dives/layouts that holds Forms(Navigator variables)
     var f_curse_adviser = document.getElementById('f_curse_adviser');
     var f_admin = document.getElementById('f_admin');
     var qualified_students = document.getElementById('qualified_students');
-    var qualified_student_form = document.qualified_student_form;
-
-    //navigation buttons
+    
+    //navigation buttons 
     var btn_adviser_navigator = document.getElementById('btn_adviser_navigator');
     var btn_admin_navigator = document.getElementById('btn_admin_navigator');
 
+    //var for Forms
+    var qualified_student_form = document.qualified_student_form;
+    
+    //var for Curse Adviser Input Fields
+    const  email_curs_adviser = document.getElementById('email_curs_adviser')
+    const  password_curs_adviser = document.getElementById('password_curs_adviser');
+    
+    // var for Admin Input Fields
+    const email_admin = document.getElementById('email_admin');
+    const password_admin = document.getElementById('password_admin');
+
+    //var for Qualified Student Input Fields
+    var student_first_name = document.getElementById('student_first_name');
+    var student_last_name = document.getElementById('student_last_name');
+    var student_email = document.getElementById('student_email');
+    var student_reg_number = document.getElementById('student_reg_number');
+    var student_phone_number = document.getElementById('student_phone_number');
+    var college = document.getElementById('college');
+    var department = document.getElementById('department');
+    var date = document.getElementById('date');
+
     //submit buttons
     var curse_adviser_submit = document.getElementById('curse_adviser_submit_btn');
-
+    var admin_submit_btn = document.getElementById('admin_submit_btn');
+    var qualified_student_submit_btn = ducument.getElementById('qualified_student_submit_btn');
     f_curse_adviser.style.display = 'block';
     f_admin.style.display = 'none';
     qualified_students.style.display = 'none'
@@ -59,4 +81,5 @@ $(document).ready(function(){
     college.addEventListener('change', (event) =>{
         loadDepartment(event.target.value);
     });
+
 });
